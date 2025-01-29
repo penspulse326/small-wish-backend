33570e6a0dd3bff6bc0bb227fdda414d79aa6493
penspulse326 <penspulse@gmail.com>
2025-01-29 15:40:50 +0800

    chore: install husky and add commitlint rules

---
+++ commitlint.config.js
@@ -0,0 +1,17 @@
+const config = {
+  extends: ['@commitlint/config-conventional'],
+  rules: {
+    'type-enum': [
+      2,
+      'always',
+      ['feat', 'fix', 'docs', 'style', 'refactor', 'chore'],
+    ],
+    'type-case': [2, 'always', 'lower-case'],
+    'type-empty': [2, 'never'],
+    'subject-empty': [2, 'never'],
+    'subject-full-stop': [2, 'never', '.'],
+    'subject-case': [0],
+  },
+};
+
+export default config;
